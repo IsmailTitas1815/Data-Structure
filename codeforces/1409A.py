7495acd714f3e1c3b0c94c40445b7dd350c1a51d
Titas <titas.sarker1234@gmail.com>
2020-09-22 19:09:36 +0600

    not solve 1409

--- codeforces/1409A.py
+++ codeforces/1409A.py
@@ -1,34 +1,44 @@
-# testcase = int(input())
-#
-# for i in range(testcase):
-#     k = 10
-#     count=0
-#     li=[1,2,3,4,5,6,7,8,9,10]
-#     a,b = list(map(int,input().split()))
-#     if a==b:
-#         print(count)
-#     else:
-#         if a<b:
-#             for j in range(k,0,-1):
-#                 while (a+j)<=b:
-#                     a = a+j
-#                     count+=1
-#                     if a==b:
-#                         break
-#
-#
-#         else:
-#             for j in range(k,0,-1):
-#                 while (a-j)>=b:
-#                     a = a-j
-#                     count+=1
-#                     if a==b:
-#                         break
-#
-#         print(count)
+testcase = int(input())
+
+for i in range(testcase):
+    k = 10
+    count=0
+    # rem= 0
+    a,b = list(map(int,input().split()))
+    if a==b:
+        print(count)
+    else:
+        if a>b:
+            max = a
+            min = b
+            diff = a-b
+        else:
+            max = b
+            min = a
+            diff = b-a
+        while True:
+            if diff>=k:
+                if diff/k < k:
+                    count+= diff//k
+                    rem = diff%k
+                    if rem!=0:
+                        count+=1
+                    break
+                else:
+                    diff = diff//k
+                    rem = diff%10
+                    count += (diff*(k-1))//k
+                    if rem!=0:
+                        count+=1
+
+            else:
+                count+=1
+                break
+        print(count)
 
 
-
+# print(917//10)
+# print(917%10)
 
 
 
@@ -38,34 +48,34 @@
 
 
 
-testcase = int(input())
-
-for i in range(testcase):
-    a,b = list(map(int,input().split()))
-    k = 10
-    count=0
-    if a==b:
-        print(count)
-    else:
-        if a<b:
-            while k>=1:
-                if (a+k)<=b:
-                    a = a+k
-                    count+=1
-                    if a==b:
-                        break
-                else:
-                    k-=1
-
-
-        else:
-            while k>=1:
-                if (a - k) >= b:
-                    a = a - k
-                    count += 1
-                    if a == b:
-                        break
-                else:
-                    k -= 1
-
-        print(count)
+# testcase = int(input())
+#
+# for i in range(testcase):
+#     a,b = list(map(int,input().split()))
+#     k = 10
+#     count=0
+#     if a==b:
+#         print(count)
+#     else:
+#         if a<b:
+#             while k>=1:
+#                 if (a+k)<=b:
+#                     a = a+k
+#                     count+=1
+#                     if a==b:
+#                         break
+#                 else:
+#                     k-=1
+#
+#
+#         else:
+#             while k>=1:
+#                 if (a - k) >= b:
+#                     a = a - k
+#                     count += 1
+#                     if a == b:
+#                         break
+#                 else:
+#                     k -= 1
+#
+#         print(count)
