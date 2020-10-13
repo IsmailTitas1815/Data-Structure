c67255a5dc3812bd4f46f0436467fe8ea2f0bd64
Titas <titas.sarker1234@gmail.com>
2020-10-13 20:14:00 +0600

    ok

---
+++ codeforces/Oct13/1372A.py
@@ -0,0 +1,9 @@
+import random
+
+testcase = int(input())
+for i in range(testcase):
+    li=[]
+    for j in range(int(input())):
+        # x = (random.randrange(1, 100))
+        li.append(1)
+    print(*li,sep=" ")
