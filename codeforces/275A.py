b9d15c4b1bb9334419a292fb38073a8913b7fea8
Titas <titas.sarker1234@gmail.com>
2020-10-14 20:58:40 +0600

    ok

---
+++ codeforces/275A.py
@@ -0,0 +1,40 @@
+# li = [[1,1,1],[1,1,1],[1,1,1],]
+# l = []
+# for i in range(3):
+#     li =  list(map(int,input().split()))[:3]
+#     l.append(li)
+#
+# for i in range(3):
+#     for j in range(3):
+#         if l[i][j]>0:
+#             if l[i][j]%2==0:
+#                 f = 0
+#             else:
+#                 f = 1
+#             if i==0 and j==0:
+#                 if f==1:
+#                     if l[0][0]==1:
+#                         l[0][0] = 0
+#                     else:
+#                         l[0][0]=1
+#
+#                     if l[0][1]==1:
+#                         l[0][1] = 0
+#                     else:
+#                         l[0][1]=1
+#
+#                     if l[1][0]==1:
+#                         l[1][0] = 0
+#                     else:
+#                         l[1][0]=1
+#
+#                     if l[1][1]==1:
+#                         l[1][1] = 0
+#                     else:
+#                         l[1][1]=1
+#
+#             elif i == 0 and j == 1:
+#                 if f == 1:
+#
+#
+#
