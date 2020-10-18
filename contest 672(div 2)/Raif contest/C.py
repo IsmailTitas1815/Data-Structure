47fff46a0989e8a82dc50f82b84afe102c1382ec
Titas <titas.sarker1234@gmail.com>
2020-10-18 15:02:00 +0600

    not ok

--- contest 672(div 2)/Raif contest/C.py
+++ contest 672(div 2)/Raif contest/C.py
@@ -1,11 +1,8 @@
 for i in range(int(input())):
     str = input()
     str = str.replace("AB","")
-    print(str)
     str = str.replace("AB","")
-    print(str)
     str = str.replace("BB","")
-    print(str)
     if len(str)>0:
         print(len(str))
     else:
