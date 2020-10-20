885bef047fa4f9568606caf3aebeb82df8b921b9
Titas <titas.sarker1234@gmail.com>
2020-10-20 20:49:13 +0600

    ok

---
+++ contest 672(div 2)/677 div 3/A.py
@@ -0,0 +1,25 @@
+for i in range(int(input())):
+    n = input()
+    l=len(n)
+    d=int(n[0])
+    s=0
+    if d==1:
+        if l==1:
+            s+=1
+        elif l==2:
+            s+=3
+        elif l==3:
+            s+=6
+        elif l==4:
+            s+=10
+    else:
+        s+=(10*(d-1))
+        if l==1:
+            s+=1
+        elif l==2:
+            s+=3
+        elif l==3:
+            s+=6
+        elif l==4:
+            s+=10
+    print(s)
