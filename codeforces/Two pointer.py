44ce519fdf03dfdfcd62b6976e14004700fd80ca
Titas <titas.sarker1234@gmail.com>
2020-11-10 04:44:42 +0600

    ok

--- codeforces/Two pointer.py
+++ codeforces/Two pointer.py
@@ -1,18 +1,18 @@
 #                           Two sum
-
-li = [1,2,456,3,5,6,4,6,8,3,9,11,6,31,6,3,9,0,12,25,19,20,25,40,41]
-li.sort()
-print(li)
-n = 50
-i = 0
-j = len(li)-1
-while li[i]+li[j]!=n:
-    if li[i]+li[j]>n:
-        j-=1
-    else:
-        i+=1
-
-print(i,j)
+#
+# li = [1,2,456,3,5,6,4,6,8,3,9,11,6,31,6,3,9,0,12,25,19,20,25,40,41]
+# li.sort()
+# print(li)
+# n = 50
+# i = 0
+# j = len(li)-1
+# while li[i]+li[j]!=n:
+#     if li[i]+li[j]>n:
+#         j-=1
+#     else:
+#         i+=1
+#
+# print(i,j)
 
 
 
@@ -27,11 +27,34 @@ print(i,j)
 # print(li)
 # diff = 5
 # i = 0
-# j = len(li)-1
+# j = 1
 # while li[j]-li[i]!=diff:
-#     if li[j]-li[i]>diff:
-#         j-=1
+#     if li[j]-li[i]<diff:
+#         j+=1
 #     else:
 #         i+=1
 #
 # print(i,j)
+
+
+#                   palindrome
+
+
+
+s = input()
+
+i = 0
+j = len(s)-1
+f = True
+while i<j:
+    if s[i]==s[j]:
+        i+=1
+        j-=1
+    else:
+        f = False
+        break
+
+if f:
+    print(f)
+else:
+    print(f)
