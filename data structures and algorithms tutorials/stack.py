1b5090e9ea8054982ab714bb09a4fc9dcb0eca42
Titas <titas.sarker1234@gmail.com>
2020-11-22 23:20:29 +0600

    ok

---
+++ data structures and algorithms tutorials/stack.py
@@ -0,0 +1,7 @@
+from collections import deque
+stack = deque()
+# print(*dir(stack),sep='\n')
+stack.append("www.google.com")
+stack.append("www.oka.com")
+stack.append("www.file.com")
+print(stack)
