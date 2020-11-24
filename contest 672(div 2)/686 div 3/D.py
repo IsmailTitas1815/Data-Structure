8f8692859228ace54860d95e913651f97b54db7f
Titas <titas.sarker1234@gmail.com>
2020-11-24 22:39:20 +0600

    not ok

--- contest 672(div 2)/686 div 3/D.py
+++ contest 672(div 2)/686 div 3/D.py
@@ -1,25 +1,46 @@
-def primeSieve(li):
-    for i in range(3,100000000000000,2):
-        li[i]= 1
+def isPrime(n):
+    if (n <= 1):
+        return False
+    if (n <= 3):
+        return True
 
-    for i in range(3,100000000000000,2):
-        if li[i]==1:
-            for j in range(i*i,100000000000000,i):
-                li[j]=0
+    if (n % 2 == 0 or n % 3 == 0):
+        return False
 
-    li[2]=1
+    i = 5
+    while (i * i <= n):
+        if (n % i == 0 or n % (i + 2) == 0):
+            return False
+        i = i + 6
+
+    return True
+
+import math
+
+def primeFactors(n):
+    li = []
+    while n % 2 == 0:
+        li.append(2)
+        n = n / 2
+
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+
+        while n % i == 0:
+            li.append(i)
+            n = n / i
+
+    if n > 2:
+        li.append(n)
+    return li
+
+for i in range(int(input())):
+    n = int(input())
+    if isPrime(n):
+        print(n)
+    else:
+        li = primeFactors(n)
 
 
-li = [0]*100000000000005
-primeSieve(li)
-n = int(input())
-if n in li:
-    flag = True
-else:
-    flag = False
 
-if flag:
-    print(n)
-else:
-    pass
+
 
