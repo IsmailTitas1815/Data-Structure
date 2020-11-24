dcd84188c8126c43da909e6db55e08a1e25ea461
Titas <titas.sarker1234@gmail.com>
2020-11-24 12:07:40 +0600

    ok

---
+++ data structures and algorithms tutorials/graph.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+
+    routes = [
+        ("Mumbai","Pune"),
+        ("Mumbai", "Surat"),
+        ("Surat", "Bangaluru"),
+        ("Pune","Hyderabad"),
+        ("Pune","Mysuru"),
+        ("Hyderabad","Bangaluru"),
+        ("Hyderabad", "Chennai"),
+        ("Mysuru", "Bangaluru"),
+        ("Chennai", "Bangaluru")
+    ]
